4a4da724c0ac467ff8c7b44793d7342441d42132
Anton Podkopaev <podkoav239@gmail.com>
2021-02-25 14:08:59 +0300

    wip

--- src/Basic.v
+++ src/Basic.v
@@ -1,6 +1,46 @@
-From mathcomp Require Import ssreflect ssrfun ssrbool eqtype ssrnat ssreflect.seq.
 Require Import String.
-(* From hahn Require Import HahnBase. *)
+From hahn Require Import Hahn.
+Require Import List.
+Import ListNotations.
+
+Set Implicit Arguments.
+
+Section Aux.
+Variable T : eqType.
+
+Fixpoint list_eqb (l l' : list T) : bool :=
+  match l, l' with
+  | [], [] => true
+  | a::l, b::l' =>
+    eq_op a b && (list_eqb l l')
+  | _, _ => false
+  end.
+
+Lemma list_eqP : Equality.axiom list_eqb.
+Proof using T.
+  red. induction x.
+  { ins. destruct y; constructor; desf. }
+  ins. desf.
+  { constructor. desf. }
+  destruct (eq_op a s) eqn:HH; subst; ins.
+  2: { constructor. intros AA. inv AA. rewrite beq_refl in HH. inv HH. }
+  specialize (IHx l).
+  apply Bool.reflect_iff in IHx.
+  apply Bool.iff_reflect.
+  etransitivity; eauto.
+  split; intros AA; desf.
+  apply hahn__beq_rewrite in HH. desf.
+Qed.
+
+Canonical Structure list_eqMixin := EqMixin list_eqP.
+Canonical Structure list_eqType := Eval hnf in EqType (list T) list_eqMixin.
+
+Fixpoint list_inb {T : eqType} x (l : list T) : bool :=
+  match l with
+  | [] => false
+  | a::l => eq_op x a || list_inb x l
+  end.
+End Aux.
 
 Definition vertex    := nat.
 Definition edge      := nat.
@@ -20,10 +60,16 @@ Canonical Structure attribute_eqMixin := EqMixin Attribute.eqP.
 Canonical Structure attribute_eqType := Eval hnf in EqType Attribute.t attribute_eqMixin.
 
 Module Scheme.
-  Definition t := seq Attribute.t.
+  Definition t := list Attribute.t.
   
-  Definition eqb (s s' : t) : bool := eqseq s s'.
+  Definition eqb (s s' : t) : bool := list_eqb _ s s'.
+
+  Lemma eqP : Equality.axiom eqb.
+  Proof. apply list_eqP. Qed.
 End Scheme.
+
+Canonical Structure scheme_eqMixin := EqMixin Scheme.eqP.
+Canonical Structure scheme_eqType := Eval hnf in EqType Scheme.t scheme_eqMixin.
 
 Module Property.
   Inductive t := 
@@ -44,13 +90,12 @@ Module Property.
 
   Lemma eqP : Equality.axiom eqb.
   Proof.
-    unfold eqb. red. intros. apply introP; desf; simpls.
-    (* TODO: is there a better way to do it in SSReflect? *)
-    { by move/EqNat.beq_nat_true->. }
-    { by move/String.eqb_eq->. }
-    all: move=>A B; inv B.
-    { rewrite PeanoNat.Nat.eqb_refl in A. simpls. }
-    rewrite String.eqb_refl in A. simpls.
+    unfold eqb. red. ins. desf; try constructor; desf.
+    all: apply Bool.iff_reflect.
+    all: symmetry; etransitivity.
+    all: try apply PeanoNat.Nat.eqb_eq; try apply String.eqb_eq.
+    all: split; intros AA; subst; auto.
+    all: inv AA.
   Qed.
 End Property.
 
@@ -61,17 +106,27 @@ Module Relation.
   Record t :=
     mk { (* sch *)
         scheme : Scheme.t;
-        data   : seq (seq Property.t);
+        data   : list (list Property.t);
       }.
-
+  
   Record wf (r : Relation.t) :=
-    { scheme_len : forall l (IN : l \in (data r)),
+    { scheme_len : forall l (IN : List.In l (data r)),
         List.length l = List.length (scheme r);
     }.
 
+  Definition bag_union r r' : option t :=
+    if Scheme.eqb (scheme r) (scheme r')
+    then Some (mk (scheme r) 
+                  (app (data r) (data r')))
+    else None.
+
   Definition union r r' : option t :=
     if Scheme.eqb (scheme r) (scheme r')
-    then mk (scheme r) 
+    then Some (mk (scheme r) 
+                  (app (data r)
+                       (filter
+                          (fun l => negb (list_inb l (data r)))
+                          (data r'))))
     else None.
 End Relation.
 
