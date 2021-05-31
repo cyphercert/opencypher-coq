e26bc518fe5e882979bbf075c129cfc1d7862de5
DenisKrivoruchko11 <den89181827071@mail.ru>
2021-05-31 11:30:13 +0300

    added relation operations

--- src/Basic.v
+++ src/Basic.v
@@ -157,38 +157,18 @@ End PropertyGraph.
 
 
 Module Pattern.
-  Record VertexPattern :=
-    v {
-        vname : string;
-        vlabels : list label;
-    }.
-  
   Inductive direction := 
   | OUT
   | IN
   | BOTH
   .
 
-  Record EdgePattern :=
-    e {
-      ename : string;
-      etypes : list Property.t;
-      ed : direction;
-      v1 : VertexPattern;
-      v2 : VertexPattern;
-    }.
-
-  (*Inductive t :=
-  | VertexPattern
-  | EdgePattern
-  .*)
-End Pattern.
-
-
-(*Module VarExpression.
   Inductive t :=
+  | vertex (name : string) (labels : list label)
+  | edge (name : string) (types : list Property.t) (dir : direction) (p : t) (wname : string) (wlabels : list label)
+  | multiedge (enames : list string) (etypes : list Property.t) (vnames : list string) (vlabels : list label) (low : nat) (up : nat)
   .
-End VarExpression.*)
+End Pattern.
 
 Module Query.
   Inductive sorting_order :=
@@ -198,39 +178,69 @@ Module Query.
 
   Inductive t :=
   | MATCH (ps : list Pattern.t)
-  | OPTIONAL_MATCH (r : option Relation.t) (p : Pattern.t)
-
-  (*| WHERE_cond (r : Relation.t) (condition : )*)
-  | WHERE (r : Relation.t) (p : Pattern.t)
-  | WHERE_NOT (r : Relation.t) (p : Pattern.t)
-
-  | RETURN (r : Relation.t) (attrs : list attribute)
-  | RETURN_DISTINCT (r : Relation.t) (attrs : list attribute)
-
-  (*| Unwind (xs : ) (x: )*)
-  | ORDER_SKIP_LIMIT (attrs : list (attribute * sorting_order)) (s : nat) (l : nat)
+  | OPTIONAL_MATCH (q : option t) (p : Pattern.t)
+  | WHERE (q : t) (p : Pattern.t)
+  | WHERE_NOT (q : t) (p : Pattern.t)
+  | RETURN (q : t) (attrs : list (attribute * string))
+  | RETURN_DISTINCT (q : t) (attrs : list (attribute * string))
+  | ORDER_SKIP_LIMIT (q : t) (attrs : list (attribute * sorting_order)) (s : nat) (l : nat)
   .
 End Query.
 
-Module GraphRelationOperation.
+Module RelationOperation. 
   Inductive t :=
-  | get_vertices (Pattern.VertexPattern)
-
-  | get_edges (v : string) (l1 : list label) (e : string) (ts : Type.t) (w:) (l2: list Label.t)
-  | undirected_get_edges (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t)
-
-  | expand_out (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t)
-  | expand_in (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t)
-  | expand_both (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t)
-
-  | transitive_expand_out (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t) (low: nat) (up: nat)
-  | transitive_expand_in (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t) (low: nat) (up: nat)
-  | transitive_expand_both (r:Relation.t) (v:) (l1: list Label.t) (e:) (t: Type.t) (w:) (l2: list Label.t) (low: nat) (up: nat)
-
-  | unwind (r:Relation) (x:)
-  | sort (attrs:) 
-  | top (s: nat) (l: nat)
-  | sort_and_top (attrs:) (s: nat) (l: nat)
-  | grouping (cs: list) (es: list)
+  | relation (r : Relation.t)
+  | select_vertices (attr_name : string) (labels : list label) (vr : t)
+  | select_edges (attr_name : string) (type : Property.t) (er : t)
+  | projection (attrs : list (string * string)) (r : t)
+  | eq_join (r1 : t) (attr1 : string) (r2 : t) (attr2 : string)
+  | natural_join (r1 : t) (r2 : t)
+  | left_outer_join (r1 : t) (r2 : t)
+  | semijoin (r1 : t) (r2 : t)
+  | anti_semijoin (r1 : t) (r2 : t)
+  | all_different (r : t)
   .
+End RelationOperation.
+
+Fixpoint graph_to_vertices_relation (pg : PropertyGraph.t) := .
+Fixpoint graph_to_edges_relation (pg : PropertyGraph.t) := .
+
+Module GraphRelationOperation.
+  Fixpoint get_vertices (attr_name : string) (labels : list label) (g : PropertyGraph.t) :=
+    projection [attr_name, attr_name] (select_vertices attr_name labels (graph_to_vertices_relation g))
+  
+  Fixpoint get_edges (name : string) (types : list Property.t) (dir : direction) (p : t) (wname : name) (wlabels : list label) 
+  (g : PropertyGraph.t) :=
+    (**)
+
+  Fixpoint transitive_get_edges (enames : list string) (etypes : list Property.t) (vnames : list string) (vlabels : list label) 
+  (low : nat) (up : nat) (g : PropertyGraph.t) :=
+    (**)
+
 End GraphRelationOperation.
+
+Fixpoint computePattern (p : Pattern.t) (g : PropertyGraph.t) :=
+  match p with
+  | vertex name labels => get_vertices name labels g
+  | edge name types dir p wname wlabels => get_edges name types dir p wname wlabels g
+  | multiedge enames etypes vnames vlabels low up => transitive_get_edges enames etypes vnames vlabels low up g
+  .
+
+Fixpoint computeQuery (q : Query.t) (g : PropertyGraph.t) :=
+  match q with
+  | MATCH ps => 
+  match ps with
+  | [] => (*Empty relation*)
+  | h :: tl => natural_join (computePattern h g) (computeQuery (MATCH tl) g)
+  .
+  | OPTIONAL_MATCH q p => left_outer_join (computeQuery q g) (computePattern p g)
+  | WHERE q p => 
+  match p with
+  | vertex name labels => select condition (computeQuery q g)
+  | _ => semijoin (computeQuery q g) (computePattern p g) 
+  .
+  | WHERE_NOT q p => anti_semijoin (computeQuery q g) (computePattern p g)
+  | RETURN q attrs => projection attrs (computeQuery q g)
+  | RETURN_DISTINCT q attrs => all_different (projection attrs (computeQuery q g))
+  (*| ORDER_SKIP_LIMIT q attrs s l => order_and_sort attrs s l (computeQuery q g)*)
+  .
