75a40b9f87e0ac39ead5e2793f0007a2f0d924a0
DenisKrivoruchko11 <den89181827071@mail.ru>
2021-07-12 11:58:15 +0300

    relocate some functions

---
+++ src/Computing.v
@@ -0,0 +1,21 @@
+Fixpoint compute_pattern (pattern : Pattern.t) (graph : PropertyGraph.t) :=
+  match pattern with
+  | Pattern.vertex vname vlabels => GRA_operations.get_vertices vname vlabels graph
+  | Pattern.edge pattern ename etype edirection wname wlabels => GRA_operations.get_edges pattern ename etype edirection wname wlabels graph
+  | Pattern.multiedge pattern enames etype low up vnames wname vlabels => GRA_operations.transitive_get_edges pattern enames etype low up vnames wname vlabels graph
+  end.
+
+Fixpoint compute_clause (clause : Clause.t) (graph : PropertyGraph.t) :=
+  match clause with
+  | MATCH patterns => match patterns with
+                      | [] => (*Empty relation*)
+                      | head :: tail => RA_operations.natural_join (compute_pattern head graph) (compute_clause (MATCH tail) graph)
+                      .
+  | WITH pexpr => RelationOperation.projection pexpr
+  end.
+
+Fixpoint compute_query (query : Query.t) (graph : PropertyGraph.t) :=
+    match query.clauses with
+    | [] => (*Empty relation*)
+    | head :: tail => RA_operations.natural_join (compute_clause head graph) (compute_query tail graph)
+    end.
