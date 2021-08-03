a68230863a98b5bf96a66eb642f0bbd5bf17b167
DenisKrivoruchko11 <denis.krvrchk@gmail.com>
2021-08-03 11:50:44 +0300

    small fixes

--- src/KleeneTranslation.v
+++ src/KleeneTranslation.v
@@ -1,16 +1,71 @@
 From Coq Require Import BinNums.
 From Coq Require Import BinInt.
+From Coq Require Import List.
+
+From OpencypherCoq Require Import Cypher.
 From RelationAlgebra Require Import syntax matrix bmx ordinal.
+Import ListNotations.
+
+From RelationAlgebra Require Import kleene boolean sups matrix.
+From OpencypherCoq Require Import PropertyGraph.
+Import PropertyGraph.
+
+Definition get_labels_matrices (n : nat) (vlab : vertex -> list label) : list (label * (ord n -> ord n -> boolean)) := .
+Definition get_types_matrices (n : nat) (elab : edge -> label) (st : edge -> vertex * vertex) : list (label * (ord n -> ord n -> boolean)) := .
+
+Definition labels := get_labels_matrices 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Fixpoint get_current_label (n : nat) (cur : )
+
+Fixpoint get_labels (n : nat) (vlab : vertex -> list label) : list (label * (ord n -> ord n -> boolean)) :=
+    let a := [] in 
+
+
+
+Definition ord0 {n}: ord (S n) := @Ord (S n) 0 eq_refl.
+
+Print ord_lt.
 
 Open Scope positive_scope.
 
 Definition var := nat.
 Definition s (_ : var) : positive := 1.
-Definition t (_ : var) : positive := 1.
-
-Definition expr1 : expr s t _ _ := e_zer 1 1.
-
-Compute monoid.ob bmx.
+Definition t (_ : var) : po
 
 Definition f' (_ : positive) :
 
