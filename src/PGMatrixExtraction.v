c71252d33ec34c124b0fdec5e7658a4da838c8a0
Derkach Ilya <ilya.derkatch@gmail.com>
2022-04-03 22:10:22 +0300

    fix bmx

--- src/PGMatrixExtraction.v
+++ src/PGMatrixExtraction.v
@@ -1,10 +1,11 @@
 Require Import PropertyGraph.
+
 Import PropertyGraph.
 From RelationAlgebra Require Import syntax matrix bmx ordinal.
+From RelationAlgebra Require Import monoid boolean prop sups bmx.
 Require Import List.
 Import ListNotations.
 Require Import String.
-Require Import Bool.
 Require Import Utils.
 Require Import BinNums.
 Require Import BinInt.
@@ -12,8 +13,9 @@ Require Import Notations.
 Require Import Ltac.
 Require Import Logic.
 Require Import Basics.
+Set Implicit Arguments.
 
-
+Local Open Scope nat_scope.
 
 Fixpoint list_of_label_to_elabel (l : list label) : list Label :=
   match l with
@@ -28,25 +30,22 @@ Fixpoint list_of_label_to_elabel (l : list label) : list Label :=
       else false.
 *)
 Print mx.
-Search mx.
-Definition pg_extract_lmatrices (n : nat) (vlab : vertex -> list label) (lbl : Label) :
-    mx bool n n :=
-  fun (x y : ord n) =>
-      if eqb_ord x y then Utils.list_inb lbl (list_of_label_to_elabel (vlab x))
-      else false.
 
-Definition ord_to_nat (n : nat) (o : ord n) : nat :=
-  match o with 
-  | Ord k _ => k
-  end.
+Search mx.
+Print bmx.
+Lemma temp (AA: bmx 0 0) : False.
+Definition pg_extract_lmatrices (n : nat) (vlab : vertex -> list label) (lbl : Label) :
+    bmx n n :=
+  fun x y =>
+      if eqb_ord x y then sup (fun i => Label_eqb lbl i) (list_of_label_to_elabel (vlab x))
+                     else false.
 
 Definition pg_extract_tmatrices (n : nat) (edges : list edge) (elab : edge -> label)
-  (st : edge -> vertex * vertex) (lbl : Label) : mx bool n n :=
-  fun (x y : ord n) => Utils.list_inb_b true (map 
-      (fun edge => andb (andb 
-                  (Nat.eqb (fst (st edge)) (ord_to_nat n x)) 
-                  (Nat.eqb (snd (st edge)) (ord_to_nat n y))) 
-                  (Label_eqb (elabel (elab edge)) lbl)) edges).
+  (st : edge -> vertex * vertex) (lbl : Label) : bmx n n :=
+  fun x y => sup (fun edge => andb (andb 
+                 (eqb_ord (ord (fst (st edge))) x) 
+                 (eqb_ord (ord (snd (st edge))) y)) 
+                 (Label_eqb (elabel (elab edge)) lbl)) edges.
 
 
 Search monoid.mor.
